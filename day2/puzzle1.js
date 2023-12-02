f18e04377211a00ca7ef7e2a5c025ba71bb5693d
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2023-12-02 17:17:32 -0500

    Solution day 2a

---
+++ day2/puzzle1.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const contents = fs.readFileSync('inputs/input.txt', 'utf8');
+
+const lines = contents.split('\n');
+lines.pop(); // Remove last empty line
+const colors = ["red", "green", "blue"];
+const color_max = [12, 13, 14];
+sum = 0;
+lines.forEach((line, index) => {
+    foundError = false;
+    for (let i = 0; i < colors.length; i++) {
+        const regex_numbers = new RegExp(`\\d+(?= ${colors[i]})`, 'g');
+        const numbers = line.match(regex_numbers);
+        if (numbers.some((number) => number > color_max[i])) {
+            foundError = true;
+            break;
+        }
+    }
+    (!foundError) && (sum += index + 1);
+});
+console.log(`${sum}`);
