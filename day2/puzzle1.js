4c1d3e06ec5a267cd4094b603e0e06a7826c3a46
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2023-12-02 17:31:30 -0500

    Fix variable declaration and initialization in puzzle1.js

--- day2/puzzle1.js
+++ day2/puzzle1.js
@@ -6,9 +6,9 @@ const lines = contents.split('\n');
 lines.pop(); // Remove last empty line
 const colors = ["red", "green", "blue"];
 const color_max = [12, 13, 14];
-sum = 0;
+let sum = 0;
 lines.forEach((line, index) => {
-    foundError = false;
+    let foundError = false;
     for (let i = 0; i < colors.length; i++) {
         const regex_numbers = new RegExp(`\\d+(?= ${colors[i]})`, 'g');
         const numbers = line.match(regex_numbers);
