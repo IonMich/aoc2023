26eb72758d53f7d33ee5073e7ee4b24091704e60
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2023-12-01 19:56:40 -0500

    improve day1a syntax

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -1,20 +1,25 @@
-def get_first_last_digits(filepath='inputs/input.txt'):
-    sum = 0
+def get_sum(filepath='inputs/input.txt'):
+    mysum = 0
     with open(filepath, 'r') as f:
-        for line in f:
-            for char in line:
-                if not char.isdigit():
-                    continue
-                firstdigit = lastdigit = char
-                break
-            for char in reversed(line):
-                if not char.isdigit():
-                    continue
-                lastdigit = char
-                break
-            linenumber = int(firstdigit + lastdigit)
-            sum += linenumber
-    return sum
+        mysum = sum(get_line_number(line) for line in f)
+    return mysum
+
+def get_line_number(line):
+    firstdigit = lastdigit = get_first_digit_from_line(line)
+    lastdigit = get_last_digit_from_line(line)
+    linenumber = int(firstdigit + lastdigit)
+    return linenumber
+
+def _get_first_digit_from_string(string):
+    for char in string:
+        if char.isdigit():
+            return char
+    
+def get_first_digit_from_line(line):
+    return _get_first_digit_from_string(line)
+        
+def get_last_digit_from_line(line):
+    return _get_first_digit_from_string(reversed(line))
 
 if __name__ == '__main__':
-    print(get_first_last_digits())
+    print(get_sum())
