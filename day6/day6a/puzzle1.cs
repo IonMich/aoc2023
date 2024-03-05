a22220a66fb542e7ef730096695a1ba82343d234
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2024-03-05 22:43:57 +0000

    clean day6a

--- day6/day6a/puzzle1.cs
+++ day6/day6a/puzzle1.cs
@@ -1,22 +1,9 @@
-﻿const int acceleration = 1;
-
-// get ../inputs/input.txt and console.log each line
-var lines = File.ReadAllLines("../inputs/input.txt");
-// foreach (var line in lines)
-// {
-//     Console.WriteLine(line);
-// }
-// It has just two lines:
-// Time:       5   42    15 ...
-// Distance: 150  140    25 ...
-
-// define the preprocessing function
-int[] Preprocess(string line)
+﻿int[] Preprocess(string line)
 {
     return line
         .Split(" ", StringSplitOptions.RemoveEmptyEntries)
         .Skip(1) // skip the first element that is just the label
-        .Select(int.Parse) // convert each element to an int
+        .Select(int.Parse)
         .ToArray();
 }
 
@@ -25,7 +12,8 @@ int IntegersBetween(double a, double b)
     return (int)Math.Ceiling(b) - (int)Math.Floor(a) - 1;
 }
 
-// store the times and distances in arrays
+const int acceleration = 1;
+var lines = File.ReadAllLines("../inputs/input.txt");
 var times = Preprocess(lines[0]);
 var distances = Preprocess(lines[1]);
 double sqrtTerm;
