93f0f74e7e9b060f6e1ca28a8816c857b6353512
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2024-03-08 20:22:26 +0000

    Solution day11b

---
+++ day11/puzzle2.r
@@ -0,0 +1,43 @@
+# read file inputs/test_input.txt
+myfile <- file("inputs/input.txt", open = "r")
+lines <- readLines(myfile)
+num_rows <- length(lines)
+num_cols <- nchar(lines[1])
+total_distance <- 0
+galaxy_coords <- c()
+for (i in 1:num_rows) {
+  line <- lines[i]
+  for (j in 1:num_cols) {
+    if (substring(line, j, j) == "#") {
+      galaxy_coords <- rbind(galaxy_coords, c(i, j))
+    }
+  }
+}
+tallies_rows <- galaxy_coords[, 1]
+tallies_cols <- galaxy_coords[, 2]
+total_galaxies <- length(tallies_rows)
+empty_rows <- setdiff(1:num_rows, tallies_rows)
+empty_cols <- setdiff(1:num_cols, tallies_cols)
+for (row in empty_rows) {
+  num_before <- sum(tallies_rows < row)
+  num_after <- total_galaxies - num_before
+  total_distance <- total_distance + num_before * num_after
+}
+for (col in empty_cols) {
+  num_before <- sum(tallies_cols < col)
+  num_after <- total_galaxies - num_before
+  total_distance <- total_distance + num_before * num_after
+}
+coeff <- 1000000
+total_distance <- (coeff - 1) * total_distance
+
+for (i in 1:(total_galaxies - 1)) {
+  for (j in (i + 1):total_galaxies) {
+    row_diff <- abs(galaxy_coords[i, 1] - galaxy_coords[j, 1])
+    col_diff <- abs(galaxy_coords[i, 2] - galaxy_coords[j, 2])
+    total_distance <- total_distance + row_diff + col_diff
+  }
+}
+options(scipen = 999)
+print(total_distance)
+close(myfile)
