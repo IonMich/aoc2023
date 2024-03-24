36175b35f17ca6c9a7f630dd080ff1dff0fd7ad0
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2024-03-24 01:04:00 -0400

    Solution day22b

--- day22/puzzle2.py
+++ day22/puzzle2.py
@@ -7,6 +7,8 @@ with open("inputs/input.txt") as file:
             for x in brick_data)
     brick_data = sorted(brick_data, key=lambda x: x[0][2])
 need_bricks = set()
+brick_supports = {}
+brick_supported_by = {}
 # ground is brick -1
 terrain = [[-1 for _ in range(10)] for _ in range(10)]
 heights = {-1: 0}
@@ -16,6 +18,9 @@ for (b_idx, (start, end)) in enumerate(brick_data):
     if sz != ez:
         terrain_below = terrain[sx][sy]
         need_bricks.add(terrain_below)
+        brick_supports[terrain_below] = brick_supports.get(terrain_below, set())
+        brick_supports[terrain_below].add(b_idx)
+        brick_supported_by[b_idx] = {terrain_below,}
         terrain[sx][sy] = b_idx
         heights[b_idx] = heights[terrain_below] + ez - sz + 1
         continue
@@ -31,6 +36,10 @@ for (b_idx, (start, end)) in enumerate(brick_data):
         elif heights[brick_below] == max_height:
             max_height_bricks.add(brick_below)
     new_height = max_height + 1
+    for brick_below in max_height_bricks:
+        brick_supports[brick_below] = brick_supports.get(brick_below, set())
+        brick_supports[brick_below].add(b_idx)
+    brick_supported_by[b_idx] = max_height_bricks.copy()
     if len(max_height_bricks) == 1:
         brick_below = max_height_bricks.pop()
         need_bricks.add(brick_below)
@@ -39,4 +48,24 @@ for (b_idx, (start, end)) in enumerate(brick_data):
         heights[b_idx] = new_height
 
 need_bricks = need_bricks-{-1,}
-print(num_bricks - len(need_bricks))
+
+def get_dependencies(root, brick_supports, brick_supported_by):
+    if root not in brick_supports:
+        return 0
+    count = 0
+    heads = set()
+    chain = {root,}
+    heads |= brick_supports[root]
+    while heads:
+        dependency = heads.pop()
+        if any(brick not in chain for brick in brick_supported_by[dependency]):
+            continue
+        chain.add(dependency)
+        count += 1
+        heads |= brick_supports.get(dependency, set()) 
+    return count
+
+total = 0
+for brick in need_bricks:
+    total += get_dependencies(brick, brick_supports, brick_supported_by)
+print(total)
