9bcc52d862af32c07480d52f09800a9c77216915
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2024-03-25 16:08:43 -0400

    Fix off by one error in day24a

--- day24/puzzle1.py
+++ day24/puzzle1.py
@@ -10,7 +10,7 @@ phase_space_data = [tuple(map(
 def get_2d_ray_intersection(state_1, state_2):
     (x1, y1, _), (vx1, vy1, _) = state_1
     (x2, y2, _), (vx2, vy2, _) = state_2
-    if vx1*vy2 in [vx2*vy1, -vx2*vy1]:
+    if (vx1*vy2 == vy1*vx2) and ((vx1*vx2 > 0 and vy1*vy2 > 0) or (vx1*vx2 < 0 and vy1*vy2 < 0)):
         return None
     y_int = (x1 - x2 - y1 * vx1/vy1 + y2 * vx2/vy2) / (vx2/vy2 - vx1/vy1)
     x_int = x1 + vx1/vy1 * (y_int - y1)
@@ -33,13 +33,15 @@ num_intersections = 0
 # max_xy = 27
 min_xy = 200000000000000
 max_xy = 400000000000000
-for i in range(num_particles):
-    for j in range(i+1, num_particles):
-        intersection = get_2d_ray_intersection(phase_space_data[i], phase_space_data[j])
-        # print(intersection)
-        if intersection is None:
-            continue
-        if is_in_box(intersection, ((min_xy, min_xy), (max_xy, max_xy))):
-            num_intersections += 1
+with open("out.txt", "w") as file:
+    for i in range(num_particles):
+        for j in range(i+1, num_particles):
+            intersection = get_2d_ray_intersection(phase_space_data[i], phase_space_data[j])
+            intersects = False
+            if (intersection is not None) and is_in_box(intersection, ((min_xy, min_xy), (max_xy, max_xy))):
+                num_intersections += 1
+                intersects = True
+
+            file.write(f"{phase_space_data[i]} {phase_space_data[j]} {intersects}\n")
 
 print(num_intersections)
